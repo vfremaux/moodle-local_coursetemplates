5cdf826cf46c82eaf8115975d5d64e45786363e3
valery <valery.fremaux@gmail.com>
2016-09-09 16:00:32 +0200

    fix version

--- version.php
+++ version.php
@@ -24,8 +24,10 @@ defined('MOODLE_INTERNAL') || die();
  */
 
 $plugin->version  = 2016031802;   // The (date) version of this plugin
-$plugin->requires = 2014050800;   // Requires this Moodle version
+$plugin->requires = 2015111100;   // Requires this Moodle version
 $plugin->component = 'local_coursetemplates';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.7.0 (Build 2016031802)';   // Requires this Moodle version
+$plugin->release = '3.0.0 (Build 2016031802)';   // Requires this Moodle version
 
+// Non moodle attributes
+$plugin->codeincrement = '3.0.0001';
