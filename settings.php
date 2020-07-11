252101ffc36ed7c1a1682125c15b3fb7506d7f7f
valery <valery.fremaux@gmail.com>
2020-07-11 17:07:54 +0200

    Update travis and verson

--- settings.php
+++ settings.php
@@ -24,6 +24,17 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
+// Settings default init.
+if (is_dir($CFG->dirroot.'/local/adminsettings')) {
+    // Integration driven code.
+    require_once($CFG->dirroot.'/local/adminsettings/lib.php');
+    list($hasconfig, $hassiteconfig, $capability) = local_adminsettings_access();
+} else {
+    // Standard Moodle code.
+    $capability = 'moodle/site:config';
+    $hasconfig = $hassiteconfig = has_capability($capability, context_system::instance());
+}
+
 if ($hassiteconfig) {
     // Needs this condition or there is error on login page.
     $settings = new admin_settingpage('local_coursetemplates', get_string('pluginname', 'local_coursetemplates'));
